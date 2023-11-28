558381eb3dbc8dbeac25100a5e4ea42cd26e1369
TheCodister <quang.nguyen26012907@hcmut.edu.vn>
2023-11-28 21:02:38 +0700

    Print page added

--- src/Pages/BuyPaper/index.jsx
+++ src/Pages/BuyPaper/index.jsx
@@ -3,6 +3,21 @@ import React, { useState } from "react";
 import { ReturnButton, PlusButton } from "../../Components";
 const BuyPaper = () => {
   const [amount, setAmount] = useState(0);
+  const updateCount = (value) => {
+    setAmount((prevCount) => {
+      const updatedCount = prevCount + value;
+      return updatedCount < 0 ? 0 : updatedCount;
+    });
+  };
+  const navigate = () => {
+    if (amount > 0) {
+      setAmount(0);
+      window,
+        open(
+          "https://sso.hcmut.edu.vn/cas/login?service=https%3A%2F%2Fbkpay.hcmut.edu.vn%2Fbkpay%2F"
+        );
+    }
+  };
   return (
     <div className="buypaper">
       <div className="buypaper-title">
@@ -14,28 +29,34 @@ const BuyPaper = () => {
           <img src="./Images/raphael_paper.svg" />
         </div>
         <div className="buypaper-button">
-          <div onClick={() => setAmount(amount - 100)}>
+          <div onClick={() => updateCount(-100)}>
             <PlusButton value="-100" />
           </div>
-          <div onClick={() => setAmount(amount - 10)}>
+          <div onClick={() => updateCount(-10)}>
             <PlusButton value="-10" />
           </div>
-          <div onClick={() => setAmount(amount - 1)}>
+          <div onClick={() => updateCount(-1)}>
             <PlusButton value="-1" />
           </div>
           <h1 className="buypaper-value">{amount}</h1>
-          <div onClick={() => setAmount(amount + 1)}>
+          <div onClick={() => updateCount(+1)}>
             <PlusButton value="+1" />
           </div>
-          <div onClick={() => setAmount(amount + 10)}>
+          <div onClick={() => updateCount(+10)}>
             <PlusButton value="+10" />
           </div>
-          <div onClick={() => setAmount(amount + 100)}>
+          <div onClick={() => updateCount(+100)}>
             <PlusButton value="+100" />
           </div>
         </div>
         <div className="buypaper-purchase">
-          <h1>Mua giấy</h1>
+          <h1
+            href="http://www.google.com"
+            target="_blank"
+            onClick={() => navigate()}
+          >
+            Mua giấy
+          </h1>
         </div>
       </div>
     </div>
--- src/Pages/Print/index.jsx
+++ src/Pages/Print/index.jsx
@@ -1,11 +1,80 @@
 import "./Print.css";
 import { ReturnButton } from "../../Components";
+import { useState } from "react";
+import axios from "axios";
 const Print = () => {
+  const [file, setFile] = useState(null);
+  const [progress, setProgress] = useState({ started: false, pc: 0 });
+  const [updateIcon, setUpdateIcon] = useState(null);
+  const [msg, setMsg] = useState("Kéo thả hoặc ấn để tải tài liệu lên");
+  const handleUpload = () => {
+    if (!file) {
+      setMsg("File trống");
+      return;
+    }
+    const fd = new FormData();
+    fd.append("file", file);
+
+    setMsg("Đang tải, vui lòng đợi giây lát...");
+    setProgress((prevState) => {
+      return { ...prevState, started: true };
+    });
+    axios
+      .post("http://httpbin.org/post", fd, {
+        onUploadProgress: (progressEvent) => {
+          setProgress((prevState) => {
+            return { ...prevState, pc: progressEvent.progress * 100 };
+          });
+        },
+        headers: {
+          "Custom-Headers": "value",
+        },
+      })
+      .then((res) => {
+        setMsg("Tải thành công!");
+        setUpdateIcon("./Images/Group 23.png");
+        console.log(res.data);
+      })
+      .catch((err) => {
+        setMsg("Tải thất bại");
+        console.error(err);
+      });
+  };
   return (
     <div className="print">
       <div className="print-title">
         <h1>In</h1>
         <ReturnButton />
+      </div>
+      <div className="print-file">
+        <div className="print-file-title">
+          <h1>Danh sách tài liệu</h1>
+        </div>
+        <div className="print-file-section">
+          <img src={updateIcon}></img>
+        </div>
+      </div>
+      <div className="print-upload">
+        <img src="./Images/upload.svg" />
+        {msg && <h1>{msg}</h1>}
+        <input
+          onChange={(e) => {
+            setFile(e.target.files[0]);
+          }}
+          type="file"
+        />
+        <p onClick={handleUpload}>Tải lên</p>
+        {progress.started && (
+          <progress
+            className="print-progress"
+            max="100"
+            value={progress.pc}
+          ></progress>
+        )}
+      </div>
+      <div className="print-confirm">
+        <h1>Xác nhận</h1>
+        <h1>Xóa</h1>
       </div>
     </div>
   );
