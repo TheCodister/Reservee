a51973260593878c8005a8f22864eb438c26b320
Quang Nguyễn <quangcuber002@gmail.com>
2023-12-03 15:21:34 +0700

    Navbar fix

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -3,25 +3,25 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <img src="./Images/Logo.png" alt="image"/>
+      <img src="./Images/Logo.png" alt="image" />
       <div className="navbar-button">
-        <div className="button">
+        <div className="navbar-butt">
           <img src="./Images/home-select.png" alt="Icon" />
           <p>Home</p>
         </div>
-        <div className="button">
+        <div className="navbar-butt">
           <img src="./Images/chart.png" alt="Icon" />
           <p>Reminder</p>
         </div>
-        <div className="button">
+        <div className="navbar-butt">
           <img src="./Images/documents.png" alt="Icon" />
           <p>History</p>
         </div>
-        <div className="button">
+        <div className="navbar-butt">
           <img src="./Images/user.png" alt="Icon" />
           <p>Account</p>
         </div>
-        <div className="button">
+        <div className="navbar-butt">
           <img src="./Images/logout.png" alt="Icon" />
           <p>Log out</p>
         </div>
