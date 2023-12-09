c7edb103ae3c137942daff2b846bf9f35ac9a178
TheCodister <quang.nguyen26012907@hcmut.edu.vn>
2023-12-09 11:23:29 +0700

    add homepage

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,56 @@ import { NavBar } from "./Components";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [dummyData] = useState([
+    {
+      id: 1,
+      name: "Ratatouile",
+      des: "Finest dining experience, with luxurious space and European cuisine. Our chef have a 20 years experience and used to work in Michelin star’s restaurant so you can ensure the quality and quantity of our product.",
+      link: `/Schedule/1`,
+      totalTable: 10,
+      totalRemain: 5,
+    },
+    {
+      id: 2,
+      name: "Phở Hậu",
+      des: "Finest dining experience, with luxurious space and European cuisine. Our chef have a 20 years experience and used to work in Michelin star’s restaurant so you can ensure the quality and quantity of our product.",
+      link: `/Schedule/2`,
+      totalTable: 10,
+      totalRemain: 5,
+    },
+    {
+      id: 3,
+      name: "Cơm Tấm Xà Bì Chưởng",
+      des: "Finest dining experience, with luxurious space and European cuisine. Our chef have a 20 years experience and used to work in Michelin star’s restaurant so you can ensure the quality and quantity of our product.",
+      link: `/Schedule/3`,
+      totalTable: 10,
+      totalRemain: 5,
+    },
+    {
+      id: 4,
+      name: "Gà Rán KFC",
+      des: "Finest dining experience, with luxurious space and European cuisine. Our chef have a 20 years experience and used to work in Michelin star’s restaurant so you can ensure the quality and quantity of our product.",
+      link: `/Schedule/4`,
+      totalTable: 10,
+      totalRemain: 5,
+    },
+    {
+      id: 5,
+      name: "Bún Đậu nước mắm",
+      des: "Finest dining experience, with luxurious space and European cuisine. Our chef have a 20 years experience and used to work in Michelin star’s restaurant so you can ensure the quality and quantity of our product.",
+      link: `/Schedule/5`,
+      totalTable: 10,
+      totalRemain: 5,
+    },
+    {
+      id: 6,
+      name: "Cơm chay",
+      des: "Finest dining experience, with luxurious space and European cuisine. Our chef have a 20 years experience and used to work in Michelin star’s restaurant so you can ensure the quality and quantity of our product.",
+      link: `/Schedule/6`,
+      totalTable: 10,
+      totalRemain: 5,
+    },
+  ]);
 
   return (
     <Router>
@@ -13,7 +62,7 @@ function App() {
         <NavBar />
         <div className="content">
           <Routes>
-            <Route exact path="/" element={<Home />} />
+            <Route exact path="/" element={<Home data={dummyData} />} />
             <Route path="/History" element={<History />} />
             <Route path="/Schedule" element={<Schedule />} />
           </Routes>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,56 @@
 import "./Home.css";
-const Home = () => {
+import { useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import { Link } from "react-router-dom";
+import { set } from "date-fns";
+const Home = (props) => {
+  const resData = props.data;
+  const [searchInput, setSearchInput] = useState("");
+  const [searchResult, setSearchResult] = useState(resData); // eslint-disable-line no-unused-vars
+  const handleChange = (e) => {
+    var lowerCase = e.target.value.toLowerCase();
+    e.preventDefault();
+    setSearchInput(lowerCase);
+    const result = resData.filter((data) => {
+      if (searchInput.length > 0) {
+        return data && data.name && data.name.toLowerCase().includes(lowerCase);
+      } else {
+        return data;
+      }
+    });
+    setSearchResult(result);
+  };
   return (
     <div className="home">
       <div className="home-title">
         <h1>Home</h1>
+        <div className="home-search">
+          <SearchIcon className="home-search-icon" />
+          <input
+            className="home-searchbar"
+            type="text"
+            placeholder="Search for your favorite restaurant"
+            onChange={handleChange}
+            value={searchInput}
+          />
+        </div>
+      </div>
+      <div className="home-menu">
+        {searchResult.map((result) => (
+          <div className="home-menu-item" key={result.id}>
+            <div className="home-menu-item-img">
+              <img src="./Images/Food.png" alt="food" />
+            </div>
+            <div className="home-menu-item-des">
+              <h2>{result.name}</h2>
+              <p>{result.des}</p>
+            </div>
+            <div className="home-menu-item-table">
+              <h2>Total table: {result.totalTable}</h2>
+              <h2>Table remain: {result.totalRemain}</h2>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
