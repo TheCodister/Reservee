9be427c92c84e3b99393867e9bf0119501133a13
TungVip <89310549+TungVip@users.noreply.github.com>
2023-12-12 07:39:07 +0700

    schedule_ver1

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,2 +1,3 @@
 export { default as NavBar } from "./NavBar";
 export { default as ReturnButton } from "./ReturnButton";
+export { default as CalendarButton } from "./CalendarButton";
--- src/Pages/Schedule/index.jsx
+++ src/Pages/Schedule/index.jsx
@@ -1,11 +1,31 @@
 import "./Schedule.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { CalendarButton } from "../../Components";
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 const Schedule = () => {
   return (
-    <div className="print">
-      <div className="print-title">
-        <h1>Reservation Schedule</h1>
+    <div className="schedule">
+      <div className="schedule-title">
+        <h1>RESERVATION SCHEDULE</h1>
+      </div>
+
+      <div className="reservation">
+        <div className="restaurant info">
+          <h2>Phở Hậu</h2>
+          <p>Số 250, đường Thành Công, Quận 1, TPHCM</p>
+        </div>
+        <div className="reservationDate">
+          <CalendarButton/>
+        </div>
+        
+        <div className="createReservation">
+
+        </div>
+
+        <table className="reservationTable">
+
+        </table>
       </div>
     </div>
   );
