332ef2d2278def5555f4974443841c796adbb624
Thuận Lê Đức <tomle@TomLe.local>
2023-12-27 20:47:18 +0700

    Test customer routes OK

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const restaurantRoutes = require('./restaurantRoutes');
-
+const customerRoutes = require('./customerRoutes');
 const router = express.Router();
 
 // Use the restaurant and customer routes
 router.use('/restaurants', restaurantRoutes);
+router.use('/customers', customerRoutes);
 
 module.exports = router;
--- server/routes/customerRoutes.js
+++ server/routes/customerRoutes.js
@@ -1,8 +1,9 @@
 // routes/customerRoutes.js
 const express = require('express');
-const db = require('../models/db'); // Replace with the correct path to your database file
+const models = require('../models'); // Adjust the path based on your folder structure
 
 const router = express.Router();
+const customerModel = models.customerModel;
 
 /**
  * @route POST /customers
@@ -10,17 +11,23 @@ const router = express.Router();
  * @body { photo_url: string, name: string, gender: string, email: string, phone_number: string }
  * @returns {Object} The created customer with ID
  */
-router.post('/', (req, res) => {
-    const { photo_url, name, gender, email, phone_number } = req.body;
-    const sql = `INSERT INTO customer (photo_url, name, gender, email, phone_number) VALUES (?, ?, ?, ?, ?)`;
+router.post("/", (req, res) => {
+	const { photo_url, name, gender, email, phone_number } = req.body;
 
-    db.run(sql, [photo_url, name, gender, email, phone_number], function(err) {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-        res.status(201).json({ id: this.lastID });
-    });
+	if (!name || !gender || !email || !phone_number) {
+		res.status(400).json({ error: "Name, gender, email, and phone number are required!" });
+		return;
+	}
+
+	const sql = `INSERT INTO customer (photo_url, name, gender, email, phone_number) VALUES (?, ?, ?, ?, ?)`;
+
+	customerModel.run(sql, [photo_url, name, gender, email, phone_number], function (err) {
+		if (err) {
+			res.status(400).json({ error: err.message });
+			return;
+		}
+		res.status(201).json({ id: this.lastID });
+	});
 });
 
 /**
@@ -28,16 +35,16 @@ router.post('/', (req, res) => {
  * @desc Get a list of all customers
  * @returns {Array} List of customers
  */
-router.get('/', (req, res) => {
-    const sql = `SELECT * FROM customer`;
+router.get("/", (req, res) => {
+	const sql = `SELECT * FROM customer`;
 
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json(rows);
-    });
+	customerModel.all(sql, [], (err, rows) => {
+		if (err) {
+			res.status(500).json({ error: err.message });
+			return;
+		}
+		res.json(rows);
+	});
 });
 
 /**
@@ -46,20 +53,25 @@ router.get('/', (req, res) => {
  * @param {Number} id - Customer's ID
  * @returns {Object} Customer data
  */
-router.get('/:id', (req, res) => {
-    const sql = `SELECT * FROM customer WHERE id = ?`;
+router.get("/:id", (req, res) => {
+	const sql = `SELECT * FROM customer WHERE id = ?`;
 
-    db.get(sql, [req.params.id], (err, row) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        if (row) {
-            res.json(row);
-        } else {
-            res.status(404).json({ error: 'Customer not found' });
-        }
-    });
+    if (!req.params.id) {
+        res.status(400).json({ error: "ID is not valid!" });
+		return;
+    }
+
+	customerModel.get(sql, [req.params.id], (err, row) => {
+		if (err) {
+			res.status(500).json({ error: err.message });
+			return;
+		}
+		if (row) {
+			res.json(row);
+		} else {
+			res.status(404).json({ error: "Customer not found" });
+		}
+	});
 });
 
 /**
@@ -68,21 +80,27 @@ router.get('/:id', (req, res) => {
  * @param {String} name - Customer's name
  * @returns {Array} List of customers matching the name
  */
-router.get('/get/name/:name', (req, res) => {
-    const { name } = req.params;
-    const sql = `SELECT * FROM customer WHERE name LIKE ?`;
+router.get("/get/name/:name", (req, res) => {
+	const { name } = req.params;
 
-    db.all(sql, [`%${name}%`], (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        if (rows.length > 0) {
-            res.json(rows);
-        } else {
-            res.status(404).json({ message: 'No customers found with that name' });
-        }
-    });
+    if (!name) {
+        res.status(400).json({ error: "Name is required!" });
+		return;
+    }
+
+	const sql = `SELECT * FROM customer WHERE name LIKE ?`;
+
+	customerModel.all(sql, [`%${name}%`], (err, rows) => {
+		if (err) {
+			res.status(500).json({ error: err.message });
+			return;
+		}
+		if (rows.length > 0) {
+			res.json(rows);
+		} else {
+			res.status(404).json({ message: "No customers found with that name" });
+		}
+	});
 });
 
 /**
@@ -92,46 +110,63 @@ router.get('/get/name/:name', (req, res) => {
  * @body { photo_url: string, name: string, gender: string, email: string, phone_number: string }
  * @returns {Object} Message and number of changed rows
  */
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
     const { photo_url, name, gender, email, phone_number } = req.body;
-    const sql = `UPDATE customer SET photo_url = ?, name = ?, gender = ?, email = ?, phone_number = ? WHERE id = ?`;
 
-    db.run(sql, [photo_url, name, gender, email, phone_number, req.params.id], function(err) {
+    // Initialize an array to hold the parts of the SQL query
+    let updates = [];
+    let queryParams = [];
+
+    // Check for each field and if it exists, add it to the updates and queryParams
+    if(photo_url !== undefined) {
+        updates.push("photo_url = ?");
+        queryParams.push(photo_url);
+    }
+
+    if(name !== undefined) {
+        updates.push("name = ?");
+        queryParams.push(name);
+    }
+
+    if(gender !== undefined) {
+        updates.push("gender = ?");
+        queryParams.push(gender);
+    }
+
+    if(email !== undefined) {
+        updates.push("email = ?");
+        queryParams.push(email);
+    }
+
+    if(phone_number !== undefined) {
+        updates.push("phone_number = ?");
+        queryParams.push(phone_number);
+    }
+
+    // If no fields are provided to update, return an error
+    if(updates.length === 0) {
+        res.status(400).json({ error: "No fields provided for update" });
+        return;
+    }
+
+    // Join the updates for the SQL query and add the customer ID at the end of queryParams
+    const sql = `UPDATE customer SET ${updates.join(", ")} WHERE id = ?`;
+    queryParams.push(req.params.id);
+
+    // Run the database query
+    customerModel.run(sql, queryParams, function(err) {
         if (err) {
             res.status(400).json({ error: err.message });
             return;
         }
         if (this.changes) {
-            res.json({ message: 'Customer updated', changes: this.changes });
+            res.json({ message: "Customer updated", changes: this.changes });
         } else {
-            res.status(404).json({ error: 'Customer not found' });
+            res.status(404).json({ error: "Customer not found" });
         }
     });
 });
 
-/**
- * @route PATCH /customers/:id
- * @desc Partially update a customer's information
- * @param {Number} id - Customer's ID
- * @body { email?: string, phone_number?: string }
- * @returns {Object} Message and number of changed rows
- */
-router.patch('/:id', (req, res) => {
-    const { email, phone_number } = req.body;
-    const sql = `UPDATE customer SET email = ?, phone_number = ? WHERE id = ?`;
-
-    db.run(sql, [email, phone_number, req.params.id], function(err) {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-        if (this.changes) {
-            res.json({ message: 'Customer updated', changes: this.changes });
-        } else {
-            res.status(404).json({ error: 'Customer not found' });
-        }
-    });
-});
 
 /**
  * @route DELETE /customers/:id
@@ -139,20 +174,20 @@ router.patch('/:id', (req, res) => {
  * @param {Number} id - Customer's ID
  * @returns {Object} Message and number of changed rows
  */
-router.delete('/:id', (req, res) => {
-    const sql = `DELETE FROM customer WHERE id = ?`;
+router.delete("/:id", (req, res) => {
+	const sql = `DELETE FROM customer WHERE id = ?`;
 
-    db.run(sql, [req.params.id], function(err) {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        if (this.changes) {
-            res.json({ message: 'Customer deleted', changes: this.changes });
-        } else {
-            res.status(404).json({ error: 'Customer not found' });
-        }
-    });
+	customerModel.run(sql, [req.params.id], function (err) {
+		if (err) {
+			res.status(500).json({ error: err.message });
+			return;
+		}
+		if (this.changes) {
+			res.json({ message: "Customer deleted", changes: this.changes });
+		} else {
+			res.status(404).json({ error: "Customer not found" });
+		}
+	});
 });
 
 module.exports = router;
