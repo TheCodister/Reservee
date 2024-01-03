ba3ca155561d99bb867f4fefe9f9b61ecda3dac5
Gia Truc Lam <truc.lamg@gmail.com>
2024-01-03 22:01:26 +0700

    "Update restaurant model."

--- server/models/restaurantModel.js
+++ server/models/restaurantModel.js
@@ -6,7 +6,10 @@ db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS restaurant (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
     photo_url VARCHAR(255), 
+    menu_urls VARCHAR(500),
     name VARCHAR(100) NOT NULL, 
+    description VARCHAR(300),
+    price_range INT CHECK(price_range >= 1 AND price_range <= 3),
     cuisine VARCHAR(50), 
     address VARCHAR(255), 
     phone_number VARCHAR(15), 
--- server/routes/restaurantRoutes.js
+++ server/routes/restaurantRoutes.js
@@ -7,7 +7,7 @@ const restaurantModel = models.restaurantModel;
 // Get all restaurants
 // Fetch all restaurants
 router.get('/', (req, res) => {
-  restaurantModel.all('SELECT * FROM restaurant', (err, rows) => {
+  restaurantModel.all('SELECT *, NULL as menu_urls FROM restaurant', (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
       return;
@@ -18,32 +18,32 @@ router.get('/', (req, res) => {
 
 // Add a new restaurant
 router.post('/', (req, res) => {
-  const {photo_url, name, cuisine, address, phone_number, seat_capacity } = req.body;
+  const {photo_url, menu_urls, name, description, price_range, cuisine, address, phone_number, seat_capacity } = req.body;
 
   // Validate required fields
-  if (!photo_url || !name || !cuisine || !address || !phone_number || !seat_capacity) {
+  if (!photo_url || !menu_urls || !name || !description || !price_range || !cuisine || !address || !phone_number || !seat_capacity) {
     res.status(400).json({ error: 'All fields are required' });
     return;
   }
 
   restaurantModel.run(
-    'INSERT INTO restaurant (photo_url, name, cuisine, address, phone_number, seat_capacity) VALUES (?, ?, ?, ?, ?, ?)',
-    [photo_url, name, cuisine, address, phone_number, seat_capacity],
+    'INSERT INTO restaurant (photo_url, menu_urls, name, description, price_range, cuisine, address, phone_number, seat_capacity) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+    [photo_url, menu_urls, name, description, price_range, cuisine, address, phone_number, seat_capacity],
     function (err) {
       if (err) {
         res.status(500).json({ error: err.message });
         return;
       }
-      res.json({ id: this.lastID, photo_url, name, cuisine, address, phone_number, seat_capacity });
+      res.json({ id: this.lastID, photo_url, menu_urls, name, description, price_range, cuisine, address, phone_number, seat_capacity });
     }
   );
 });
 
-// API endpoint to get a reservation by restaurant ID
+// API endpoint to get a restaurant by restaurant ID
 router.get('/:restaurant_id', (req, res) => {
   const restaurant_id = req.params.restaurant_id;
 
-  reservationModel.all(
+  restaurantModel.all(
       `
       SELECT *
       FROM restaurant
@@ -62,6 +62,34 @@ router.get('/:restaurant_id', (req, res) => {
   );
 });
 
+// API endpoint to get a menu by restaurant ID
+router.get('/menus/:restaurant_id', (req, res) => {
+  const restaurant_id = req.params.restaurant_id;
+
+  restaurantModel.all(
+      `
+      SELECT json(menu_urls) AS menu_json
+      FROM restaurant 
+      WHERE id = ?
+  `,
+      [restaurant_id],
+      (err, rows) => {
+      if (err) {
+          console.error(err);
+          res.status(500).json({ error: 'Internal Server Error' });
+          return;
+      }
+      if (rows.length > 0) {
+        const menuJsonString = rows[0].menu_json;
+        const menuJson = JSON.parse(menuJsonString);
+        res.json({ menus: menuJson });
+      } else {
+        res.status(404).json({ error: 'Restaurant not found' });
+      }
+    }
+  );
+});
+
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword;
 
@@ -72,7 +100,8 @@ router.get('/search', (req, res) => {
 
   const query = `
     SELECT * FROM restaurant
-    WHERE name LIKE ? COLLATE NOCASE OR address LIKE ? COLLATE NOCASE OR cuisine LIKE ? COLLATE NOCASE
+    WHERE name LIKE ? COLLATE NOCASE OR address LIKE ? COLLATE NOCASE 
+    OR cuisine LIKE ? COLLATE NOCASE OR description LIKE ? COLLATE NOCASE
   `;
 
   const searchTerm = `%${keyword}%`; // Add wildcard for partial matches
