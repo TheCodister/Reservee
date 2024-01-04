d1a3fde69e53a20ade404fbc68bd094266b220c1
TungVip <89310549+TungVip@users.noreply.github.com>
2024-01-04 12:34:04 +0700

    small adjust

--- client/src/Components/index.jsx
+++ client/src/Components/index.jsx
@@ -2,3 +2,4 @@ export { default as NavBar } from "./NavBar";
 export { default as ReturnButton } from "./ReturnButton";
 export { default as CalendarButton } from "./CalendarButton";
 export { default as FormModal } from "./FormModal";
+export { default as Menu } from "./Menu";
